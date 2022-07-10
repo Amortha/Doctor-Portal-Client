0af10b6bb1afde5c676e22f5e1e1873a0bb43595
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-07-10 21:03:24 +0600

    commit

--- src/Pages/Home/Testimonials.js
+++ src/Pages/Home/Testimonials.js
@@ -1,10 +1,53 @@
 import React from 'react';
-
+import quote from '../../assets/icons/quote.svg'
+import pepole1 from '../../assets/images/people1.png'
+import pepole2 from '../../assets/images/people1.png'
+import pepole3 from '../../assets/images/people1.png'
+import Review from './Review';
 const Testimonials = () => {
+    const reviews =[
+        {
+        _id:1,
+        name:'Winson Herry',
+        reviews:'',
+        location:'califonia',
+        img: pepole1
+        },
+        {
+        _id:2,
+        name:'Winson Herry',
+        reviews:'',
+        location:'califonia',
+        img: pepole2
+        },
+        {
+        _id:3,
+        name:'Winson Herry',
+        reviews:'',
+        location:'califonia',
+        img: pepole3
+        },
+    ]
     return (
-        <div>
-            
+       <section className='my-28'>
+        <div className='flex justify-between'>
+          <div>
+          <h4 className='text-xl text-primary font-bold'>Testimonials</h4>
+         <h2 className='text-3xl'>What our patients say</h2>
+            </div>  
+          <div>
+            <img src={quote} className="w-24 lg:w-48" alt=""/>
+            </div>  
         </div>
+        <div className='grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+            {
+                reviews.map(review=><Review
+                key={review._id}
+                review={review}
+                ></Review>)
+            }
+        </div>
+       </section>
     );
 };
 
