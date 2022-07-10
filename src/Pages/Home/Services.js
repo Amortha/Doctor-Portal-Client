614b1a0a11cc43bb2559e2527f2c01c89f667b04
Amortha kumar shill <amitsharma1998k@gmail.com>
2022-07-10 12:57:34 +0600

    commit

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Services = () => {
+    const Services = [
+        {
+          _id:1,
+          name: 'Fluoraide Treatment',  
+        }
+    ]
+    return (
+        <div className='my-28'>
+          <div className='text-center '>
+          <h3 className='text-primary text-xl font-bold uppercase'>our Services</h3> 
+          <h2 className='text-4xl'>Services We Provide</h2> 
+          </div>
+          <div>
+
+          </div>
+        </div>
+    );
+};
+
+export default Services;
